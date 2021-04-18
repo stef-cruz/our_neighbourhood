6ab27bc2b6c590c0fcea264a5af891e70d546239
Stef Cruz <crzstefanie@gmail.com>
2021-04-18 15:32:35 +0100

    Create allauth and base template

--- our_neighbourhood/settings.py
+++ our_neighbourhood/settings.py
@@ -66,7 +66,10 @@ ROOT_URLCONF = 'our_neighbourhood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -150,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
