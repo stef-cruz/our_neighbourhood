d4a8115179dd156b57fb8d3da6ce9ce5410e94a4
Stef Cruz <crzstefanie@gmail.com>
2021-05-04 21:49:18 +0100

    Change layout of crispy form under account

--- profiles/forms.py
+++ profiles/forms.py
@@ -25,7 +25,3 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-
-            self.fields[field].widget.attrs['class'] = ('border-black '
-                                                        'rounded-0 '
-                                                        'profile-form-input')
