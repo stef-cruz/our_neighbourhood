227fc0357496977d0f41a6da145d6261328cf149
Stef Cruz <crzstefanie@gmail.com>
2021-05-05 19:53:28 +0100

    Rename variable on settings.py

--- our_neighbourhood/settings.py
+++ our_neighbourhood/settings.py
@@ -217,10 +217,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # AWS
 if 'USE_AWS' in os.environ:
+    # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'ci-milestone4'
-    AWS_REGION_NAME = 'eu-west-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_KEY_ACCESS = os.environ.get('AWS_SECRET_KEY_ACCESS')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY_ACCESS')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
