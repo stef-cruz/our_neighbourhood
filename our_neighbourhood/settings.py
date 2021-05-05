3bfa843e21f5e178a9a3a1296d85a8d417787fc9
Stef Cruz <crzstefanie@gmail.com>
2021-05-05 17:51:57 +0100

    Prep settings.py for deployment and add Procfile

--- our_neighbourhood/settings.py
+++ our_neighbourhood/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
    import env
@@ -27,10 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+development = os.environ.get("DEVELOPMENT", False)
+DEBUG = development
 
-ALLOWED_HOSTS = []
-
+if development:
+    ALLOWED_HOSTS = [os.environ.get('LOCALHOST')]
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -148,12 +152,17 @@ WSGI_APPLICATION = 'our_neighbourhood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if development:
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       }
+   }
+else:
+   DATABASES = {
+       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+   }
 
 
 # Password validation
