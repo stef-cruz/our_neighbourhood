f7157b70d90cd3bac84f16b3c32521e5f02c282b
Stef Cruz <crzstefanie@gmail.com>
2021-05-06 20:53:16 +0100

    Add toasts

--- our_neighbourhood/settings.py
+++ our_neighbourhood/settings.py
@@ -217,6 +217,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # AWS
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'ci-milestone4'
     AWS_S3_REGION_NAME = 'eu-west-1'
