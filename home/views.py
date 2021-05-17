b7b0a81c204efa277dafb7741e8cdfaba58e382a
Stef Cruz <crzstefanie@gmail.com>
2021-05-17 21:11:59 +0100

    Add view to return all events on home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from events.models import Event
+
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+
+    events = Event.objects.all()
+
+    template = 'home/index.html'
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, template, context)
