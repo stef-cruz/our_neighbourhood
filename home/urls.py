c9b97b424429175e665939ace426c17eb762dc54
Stef Cruz <crzstefanie@gmail.com>
2021-05-31 20:02:00 +0100

    Create FAQ page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('faq/', TemplateView.as_view(template_name='home/faq.html'), name='faq'),
 ]
