67370c9cc4ff767d845060924d7b8ebb60e625d1
Stef Cruz <crzstefanie@gmail.com>
2021-06-17 21:05:30 +0100

    Add unit test profile & home app

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,48 +1,29 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.test.client import Client
 
 
 class TestProfileView(TestCase):
     """
-    Test profile views - Happy path
+    Test profile views
 
-    def profile
     1. Set up the scenario
-    2. Check update profile functionality
-    3. Assert
-
-    def delete_profile
-    Using the same scenario:
-    1. Check delete profile functionality
-    2. Assert
+    2. Test update profile functionality
+    3. Test delete profile functionality
 
     """
 
-    @classmethod
     def setUp(self):
-        super().setUpClass()
-        username = 'testuser'
-        password = 'testpass'
-
-        User = get_user_model()
-        self.user = User.objects.create_user(username, password=password)
-
-        logged_in = self.client.login(username=username, password=password)
-        self.assertTrue(logged_in)
+        self.client = Client()
+        self.user = User.objects.create_superuser('admin', 'admin@email.com', 'adminpass')
 
     def test_update_profile(self):
-        # Update profile
-        self.response = self.client.post('/profile/', data= {'full_name': '', 'bio': 'test update bio'})
-        print(self.response)
-        # Assert response 200
-        self.assertEqual(self.response.status_code, 200)
-
+        self.client.login(username='admin', password='adminpass')
+        response = self.client.post('/profile/', data={'full_name': 'test update name', 'bio': 'test update bio'})
+        self.assertEqual(response.status_code, 200)
 
     def test_delete_profile(self):
-        # Delete profile
-        self.response = self.client.get('/profile/delete_profile/')
-
-        # Assert it was deleted
-        self.assertEqual(self.response.status_code, 404)
-
+        self.client.login(username='admin', password='adminpass')
+        user_to_delete = User.objects.get(username='admin')
+        response = self.client.get(f'/profile/delete_profile/{user_to_delete}')
+        self.assertRedirects(response, '/')
