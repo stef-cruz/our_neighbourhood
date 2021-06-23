c839a3160f89df13f01cfebab92d632cc17c3a86
Stef Cruz <crzstefanie@gmail.com>
2021-06-23 21:56:11 +0100

    Add back SQLite DB code

--- our_neighbourhood/settings.py
+++ our_neighbourhood/settings.py
@@ -147,9 +147,18 @@ WSGI_APPLICATION = 'our_neighbourhood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+
+if development:
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       }
+   }
+else:
+   DATABASES = {
+       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+   }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
